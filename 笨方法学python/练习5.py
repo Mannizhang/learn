e7691ad8c8d532d72c574a3d971d6e3831c19156
Mari <1109878997man@gamil.com>
2016-08-28 00:22:04 +0800

    8.28

--- 笨方法学python/练习5.py
+++ 笨方法学python/练习5.py
@@ -6,4 +6,15 @@ my_eyes='Blue'
 my_teeth='White'
 my_hair='Brown'
 
-print("Let's talk about %s") % my_name
+en="Let's talk about this %s"
+print(en % my_name)
+en2="He's %d inches tall."
+print(en2% my_height)
+en3="He's %d pounds heavy."
+print(en3%my_weight)
+print("Actually that's not too heavy")
+en4="He's got %s eyes." or "%s hair"
+print(en4%my_eyes,en4%my_hair)
+en5="his teeth are usually %s depending on the coffee."
+print(en5%my_teeth)
+
