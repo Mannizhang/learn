b6a01ead2947549293061c854078f503bf148e4c
Mari <1109878997man@gamil.com>
2016-08-30 22:56:17 +0800

    8.30

---
+++ 笨方法学python/练习17.py
@@ -0,0 +1,6 @@
+#import shutil
+#shutil.copyfile(‘listfile.py’, ‘d:/test.py’) #复制文件
+#shutil.copystat(src,dst) #拷贝文件，连同文件的stat一起拷贝
+#shutil.copytree(src,dst) #拷贝目录，拷贝之前dst必须不存在
+#shutil.copy2(src,dst)#拷贝文件，连同文件的stat信息一起拷贝，不能拷贝目录
+#不会用
