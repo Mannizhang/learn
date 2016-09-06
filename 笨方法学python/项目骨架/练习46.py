e9eb030392c54a1da5bf727fdcdab1bcd7f874cf
Mari <1109878997man@gamil.com>
2016-09-06 20:34:11 +0800

    9.6

---
+++ 笨方法学python/项目骨架/练习46.py
@@ -0,0 +1,14 @@
+mkdir projects
+cd projects
+mkdir skeleton
+cd skeleton
+mkdir bin
+mkdir Name
+mkdir tests
+mkdir docs
+
+new-item-type file NAME/_init_.py
+new-item-type file tests/_init_.py
+
+
+
