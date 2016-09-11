4b110dd66fe72c8e857af3f2ae50e9a739a0c7be
Mari <1109878997man@gamil.com>
2016-09-11 20:46:17 +0800

    9.11

---
+++ flask web/第三章/src/hello2.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index2.html")
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user2.html', name=name)
+
+
+@app.route('/ifelse/<username1>')
+def ifelse(username1):
+    return render_template("ifelse2.html", name=username1)
+
+
+@app.route('/base/<use>')
+def base(use):
+    return render_template("base2.html", name=use)
+
+
+@app.route('/rebase/<reuse>')
+def rebase(reuse):
+    return render_template("rebase.html", name=reuse)
+
+from flask.ext.bootstrap import Bootstrap
+
+if __name__ == '__main__':
+    app.run(debug=True)
