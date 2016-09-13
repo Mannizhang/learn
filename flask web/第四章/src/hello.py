33408eab780c5fdb2ebfb3b25ac4191c6fb75863
Mari <1109878997man@gamil.com>
2016-09-13 22:53:51 +0800

    9.13

--- flask web/第四章/src/hello.py
+++ flask web/第四章/src/hello.py
@@ -2,31 +2,39 @@ from flask import Flask, render_template
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-
-{% import "bootstrap/wtf.html" as wtf %}
-{{ wtf.quick_form(form) }}
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-app.config['SECRET_KEY'] = 'hard to guess string'
 
+@app.route('/base/<use>')
+def base(use):
+    return render_template("base.html", name=use)
+
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+@app.route('/',methods=['GET','POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',form=form,name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run('0.0.0.0')
